ea1f66fe76d0524ae4c9f25996817e55a3ec0aa2
Skitt <samuelponceluna@gmail.com>
2024-05-20 16:54:46 +0200

    Update convert_to_pdf.py

--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -1,11 +1,19 @@
-from docx2pdf import convert
+import subprocess
 import sys
+import os
 
 # Obtén la ruta del archivo DOCX desde los argumentos de la línea de comandos
 input_docx_path = sys.argv[1]
 
-# Realiza la conversión del archivo DOCX a PDF
+# Define la ruta de salida del archivo PDF
 output_pdf_path = input_docx_path.replace('.docx', '.pdf')
-convert(input_docx_path, output_pdf_path)
 
-print(output_pdf_path)  # Imprime la ruta del archivo PDF generado
+# Realiza la conversión del archivo DOCX a PDF usando unoconv
+try:
+    subprocess.run(['unoconv', '-f', 'pdf', input_docx_path], check=True)
+    if os.path.exists(output_pdf_path):
+        print(output_pdf_path)  # Imprime la ruta del archivo PDF generado
+    else:
+        print("Error: El archivo PDF no se generó.")
+except subprocess.CalledProcessError as e:
+    print(f"Error durante la conversión: {e}")
